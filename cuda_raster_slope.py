ae5d42da60480d6677549e4a050d3cbdfb3a474b
Lukas Winiwarter <lukas.winiwarter@ubc.ca>
2022-12-09 23:12:31 -0800

    added comparisons

--- cuda_raster_slope.py
+++ cuda_raster_slope.py
@@ -7,29 +7,51 @@ import numpy as np
 import math
 
 
-
-@cuda.jit
-def get_slope_cuda(arrx, arry, Sx, SxSx, out):
-    cx, cy = cuda.grid(2)
-    n = len(arrx)
+@numba.jit(nopython=True, nogil=True)
+def get_slope_numba_simple(arrx, arry, Sx, Sx2, SxSx):
+    n = arrx.shape[0]
     Sy = 0
-    # Sx = 0
-    Sxy = 0
     Sx2 = 0
-    for itemx, itemy in zip(arrx, arry[cx, cy]):
+    Sxy = 0
+    for itemx, itemy in zip(arrx, arry):
         Sxy += itemx * itemy
         Sx2 += itemx * itemx
-        # Sx += itemx
         Sy += itemy
-    out[cx, cy]= (n*Sxy - Sx * Sy) / (n*Sx2 - SxSx)
+    return (n*Sxy - Sx * Sy) / (n*Sx2 - SxSx)
+
+
+def get_slope_simple(arrx, arry, Sx, Sx2, SxSx):
+    n = arrx.shape[0]
+    Sy = 0
+    Sxy = 0
+    for itemx, itemy in zip(arrx, arry):
+        Sxy += itemx * itemy
+        Sy += itemy
+    return (n*Sxy - Sx * Sy) / (n*Sx2 - SxSx)
+
+def get_slope_polyfit(arr_vals):
+    return np.polyfit(np.arange(len(arr_vals)), arr_vals, deg=1)[0]
+
+@cuda.jit(device=True)
+def get_slope_cuda_simple(arrx, arry, Sx, Sx2, SxSx):
+    n = arrx.shape[0]
+    Sy = 0
+    Sxy = 0
+    for itemx, itemy in zip(arrx, arry):
+        Sxy += itemx * itemy
+        Sy += itemy
+    return (n*Sxy - Sx * Sy) / (n*Sx2 - SxSx)
+
+@cuda.jit
+def get_slope_cuda(arrx, arry, Sx, Sx2, SxSx, out):
+    cx, cy = cuda.grid(2)
+    out[cx, cy] = get_slope_cuda_simple(arrx, arry[cx, cy], Sx, Sx2, SxSx)
 
 
 if __name__ == '__main__':
     np.random.seed(42)
-    testdata = np.random.randint(1000, 5000, (8000, 30000, 10))#.astype(float)
-    x = np.arange(6)
-    Sx = np.sum(x)
-    SxSx = Sx * Sx
+    testdata = np.random.randint(1000, 5000, (8000, 3000, 10))#.astype(float)
+    x = np.arange(testdata.shape[-1]).astype(np.int64)
     # an_array = np.zeros(testdata.shape[:-1], dtype=float)
 
     print("Data ready.")
@@ -42,14 +64,58 @@ if __name__ == '__main__':
 
     t0 = time.time()
     stream = cuda.stream()
+    Sx = int(sum(x))
+    SxSx = Sx * Sx
+    Sx2 = int(sum(np.square(x)))
 
-    x = cuda.to_device(x)
-    Sx = cuda.to_device(Sx)
-    SxSx = cuda.to_device(SxSx)
-    testdata = cuda.to_device(np.ascontiguousarray(testdata)) # transfer to GPU manually, so it won't be transferred back
-    an_array = cuda.device_array(testdata.shape[:-1], dtype=float)  # create on GPU directly, no need for copying
+    x_dev = cuda.to_device(x)
+    testdata_dev = cuda.to_device(np.ascontiguousarray(testdata)) # transfer to GPU manually, so it won't be transferred back
+    an_array = cuda.device_array(testdata_dev.shape[:-1], dtype=float)  # create on GPU directly, no need for copying
 
-    get_slope_cuda[blockspergrid, threadsperblock, stream](x, testdata, Sx, SxSx, an_array)
-    print(an_array.copy_to_host())
+    get_slope_cuda[blockspergrid, threadsperblock, stream](x_dev, testdata_dev, Sx, Sx2, SxSx, an_array)
+    out_cuda = an_array.copy_to_host()
     t1 = time.time()
-    print(f"Took {(t1-t0)}s. (Array dim: {testdata.shape}")
+    print(f"Cuda:                   took {(t1-t0):7.4f}s. (Array dim: {testdata.shape})")
+    del an_array, x_dev, testdata_dev  # free GPU memory
+
+    t0 = time.time()
+    n = testdata.shape[-1]
+    x = np.arange(n)
+    Sx = np.sum(x)
+    SxSx = Sx * Sx
+    Sx2 = np.sum(np.square(x))
+    Sy = np.sum(testdata, axis=-1)
+    sXY = np.einsum('ijk,k', testdata, x)
+    out_vec = (n * sXY - Sx * Sy) / (n * Sx2 - SxSx)
+    t1 = time.time()
+    print(f"Vectorized:             took {(t1-t0):7.4f}s. (Array dim: {testdata.shape}) "
+          f"- are results the same? {np.allclose(out_vec, out_cuda)}")
+
+    t0 = time.time()
+    n = testdata.shape[-1]
+    x = np.arange(n)
+    Sx = np.sum(x)
+    SxSx = Sx * Sx
+    Sx2 = np.sum(np.square(x))
+    out_numba = np.apply_along_axis(lambda data: get_slope_numba_simple(x, data, Sx, Sx2, SxSx), -1, testdata)
+    t1 = time.time()
+    print(f"Numba apply along axis: took {(t1-t0):7.4f}s. (Array dim: {testdata.shape}) "
+          f"- are results the same? {np.allclose(out_numba, out_cuda)}")
+
+    t0 = time.time()
+    n = testdata.shape[-1]
+    x = np.arange(n)
+    Sx = np.sum(x)
+    SxSx = Sx * Sx
+    Sx2 = np.sum(np.square(x))
+    out_std = np.apply_along_axis(lambda data: get_slope_simple(x, data, Sx, Sx2, SxSx), -1, testdata)
+    t1 = time.time()
+    print(f"Std. apply along axis:  took {(t1-t0):7.4f}s. (Array dim: {testdata.shape}) "
+          f"- are results the same? {np.allclose(out_std, out_cuda)}")
+
+
+    t0 = time.time()
+    out_poly = np.apply_along_axis(get_slope_polyfit, -1, testdata)
+    t1 = time.time()
+    print(f"Polyfit:                took {(t1-t0):7.4f}s. (Array dim: {testdata.shape}) "
+          f"- are results the same? {np.allclose(out_vec, out_cuda)}")
